2fbe4c4f0cc7def2017d9d1810879e6294ba1b49
Kelly Young <ag43316@LC02Z45M5LVDQ.US.AD.WELLPOINT.COM>
2019-09-06 14:52:42 -0400

    Finished getters and setters

--- angular-data-flow-tutorial/src/app/binding/binding.component.ts
+++ angular-data-flow-tutorial/src/app/binding/binding.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from "@angular/core";
 export class BindingComponent implements OnInit {
   userInput: string;
   userInput2: string;
+  private _userInput3: string;
 
   constructor() { }
 
@@ -16,10 +17,22 @@ export class BindingComponent implements OnInit {
   clickEventExample = () => {
     alert("You just triggered the click event.");
   }
+
   secretWord(updatedValue) {
     this.userInput2 = updatedValue;
     if (updatedValue === "chinchilla") {
       alert("You typed the secret word!")
+    }
+  }
+
+  get userInput3(): string {
+    return this._userInput3;
+  }
+
+  set userInput3(value: string) {
+    this._userInput3 = value;
+    if (value === "knock knock") {
+      alert("Who's there?");
     }
   }
 }
