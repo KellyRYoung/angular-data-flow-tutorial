a70598d951d4652cd1603467e09fe7d0cd04f934
Kelly Young <ag43316@LC02Z45M5LVDQ.US.AD.WELLPOINT.COM>
2019-09-07 16:42:06 -0400

    Finished template reference variables

--- angular-data-flow-tutorial/src/app/parent-child/parent-child.component.ts
+++ angular-data-flow-tutorial/src/app/parent-child/parent-child.component.ts
@@ -9,9 +9,14 @@ export class ParentChildComponent implements OnInit {
 
   userLoggedIn: boolean = false;
 
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  greetUser() {
+    alert("Hello! I hope you're learning a lot!");
   }
 
 }
